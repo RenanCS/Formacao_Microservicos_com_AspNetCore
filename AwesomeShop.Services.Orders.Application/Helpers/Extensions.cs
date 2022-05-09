a1526adefb9397967641d546657084eb488a8f24
renan <renan@interprocess.com.br>
2022-05-09 19:04:54 -0300

    feat: add extensions to handlers

---
+++ AwesomeShop.Services.Orders.Application/Helpers/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using AwesomeShop.Services.Orders.Application.Dtos.InputModels;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AwesomeShop.Services.Orders.Application.Helpers
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddHandlers(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(AddOrderCommand));
+            return services;
+        }
+    }
+}
