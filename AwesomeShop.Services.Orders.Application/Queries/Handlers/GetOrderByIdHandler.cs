195305b7393f45b997188081010418ae542c6de3
renan <renan@interprocess.com.br>
2022-05-09 18:20:14 -0300

    feat: add handler to get order by id

---
+++ AwesomeShop.Services.Orders.Application/Queries/Handlers/GetOrderByIdHandler.cs
@@ -0,0 +1,26 @@
+﻿using AwesomeShop.Services.Orders.Application.Dtos.InputModels;
+using AwesomeShop.Services.Orders.Application.Dtos.ViewModels;
+using AwesomeShop.Services.Orders.Core.Repositories;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AwesomeShop.Services.Orders.Application.Queries.Handlers
+{
+    public class GetOrderByIdHandler : IRequestHandler<GetOderByIdQuery, OrderViewModel>
+    {
+        private readonly IOrderRepository _orderRepository;
+        public GetOrderByIdHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+        public async Task<OrderViewModel> Handle(GetOderByIdQuery request, CancellationToken cancellationToken)
+        {
+            var order = await _orderRepository.GetByIdAsync(request.Id);
+
+            var orderViewModel = OrderViewModel.FactoryOrderViewModelFromEntity(order);
+
+            return orderViewModel;
+        }
+    }
+}
