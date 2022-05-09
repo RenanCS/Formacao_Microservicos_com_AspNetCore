3bcee49f1c5dbcdc108dbcc7b5b028d58cb3a9de
renan <renan@interprocess.com.br>
2022-05-09 18:20:39 -0300

    feat: add input view query to get order by id

---
+++ AwesomeShop.Services.Orders.Application/Dtos/InputModels/GetOderByIdQuery.cs
@@ -0,0 +1,17 @@
+﻿using AwesomeShop.Services.Orders.Application.Dtos.ViewModels;
+using MediatR;
+using System;
+
+namespace AwesomeShop.Services.Orders.Application.Dtos.InputModels
+{
+    public class GetOderByIdQuery : IRequest<OrderViewModel>
+    {
+        public GetOderByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; private set; }
+
+    }
+}
