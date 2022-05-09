6854e8d15a81b69d4251dbb47f911ead9a821f78
renan <renan@interprocess.com.br>
2022-05-09 19:03:34 -0300

    feat: add logic addOrderHandler

--- AwesomeShop.Services.Orders.Application/Commands/Handlers/AddOrderHandler.cs
+++ AwesomeShop.Services.Orders.Application/Commands/Handlers/AddOrderHandler.cs
@@ -1,6 +1,27 @@
-﻿namespace AwesomeShop.Services.Orders.Application.Commands.Handlers
+﻿using AwesomeShop.Services.Orders.Application.Dtos.InputModels;
+using AwesomeShop.Services.Orders.Core.Repositories;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AwesomeShop.Services.Orders.Application.Commands.Handlers
 {
-    public class AddOrderHandler
+    public class AddOrderHandler : IRequestHandler<AddOrderCommand, Guid>
     {
+        private readonly IOrderRepository _orderRepository;
+        public AddOrderHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<Guid> Handle(AddOrderCommand request, CancellationToken cancellationToken)
+        {
+            var order = request.FactoryAddOrderCommandToOrderEntity();
+
+            await _orderRepository.AddAsync(order);
+
+            return order.Id;
+        }
     }
 }
