86f5ce9cf7e78e378fe1087b9d6dcc037a557ea8
renan <renan@interprocess.com.br>
2022-05-16 21:10:54 -0300

    feat: Add configuration options mongo

---
+++ AwesomeShop.Services.Orders.Infrastructure/db/mongo/MongoDbOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace AwesomeShop.Services.Orders.Infrastructure.db.mongo
+{
+    public class MongoDbOptions
+    {
+        public MongoDbOptions(string database, string connectionString)
+        {
+            Database = database;
+            ConnectionString = connectionString;
+        }
+
+        public string Database { get; private set; }
+        public string ConnectionString { get; private set; }
+
+    }
+}
