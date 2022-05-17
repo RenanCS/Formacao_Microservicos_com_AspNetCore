a885614efa2d57292467aa80a259392482737fc4
renan <renan@interprocess.com.br>
2022-05-17 11:36:03 -0300

    chore: add register event to send message bus order-service

--- AwesomeShop.Services.Orders.Application/Commands/Handlers/AddOrderHandler.cs
+++ AwesomeShop.Services.Orders.Application/Commands/Handlers/AddOrderHandler.cs
@@ -1,5 +1,7 @@
 ﻿using AwesomeShop.Services.Orders.Application.Dtos.InputModels;
 using AwesomeShop.Services.Orders.Core.Repositories;
+using AwesomeShop.Services.Orders.Infrastructure;
+using AwesomeShop.Services.Orders.Infrastructure.MessageBus;
 using MediatR;
 using System;
 using System.Threading;
@@ -10,9 +12,11 @@ namespace AwesomeShop.Services.Orders.Application.Commands.Handlers
     public class AddOrderHandler : IRequestHandler<AddOrderCommand, Guid>
     {
         private readonly IOrderRepository _orderRepository;
-        public AddOrderHandler(IOrderRepository orderRepository)
+        private readonly IMessageBusCllient _messageBusCllient;
+        public AddOrderHandler(IOrderRepository orderRepository, IMessageBusCllient messageBusClient)
         {
             _orderRepository = orderRepository;
+            _messageBusCllient = messageBusClient;
         }
 
         public async Task<Guid> Handle(AddOrderCommand request, CancellationToken cancellationToken)
@@ -20,6 +24,14 @@ namespace AwesomeShop.Services.Orders.Application.Commands.Handlers
             var order = request.FactoryAddOrderCommandToOrderEntity();
 
             await _orderRepository.AddAsync(order);
+
+            foreach (var @event in order.Events)
+            {
+                // OrderCreated = order-created
+                var routingKey = @event.GetType().Name.ToDashCase();
+
+                _messageBusCllient.Publish(@event, routingKey, "order-service");
+            }
 
             return order.Id;
         }
