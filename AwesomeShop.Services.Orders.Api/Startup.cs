210cceabac580b76bdd70727060f1c425d7ae2af
renan <renan@interprocess.com.br>
2022-05-16 21:10:01 -0300

    chore: Change configure services add mongodb

--- AwesomeShop.Services.Orders.Api/Startup.cs
+++ AwesomeShop.Services.Orders.Api/Startup.cs
@@ -1,4 +1,5 @@
 using AwesomeShop.Services.Orders.Application.Helpers;
+using AwesomeShop.Services.Orders.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -20,6 +21,7 @@ namespace AwesomeShop.Services.Orders.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMongo();
             services.AddInfrastructure();
             services.AddHandlers();
             services.AddControllers();
