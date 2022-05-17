cc052a80a07fde5424b7631d2d7b02a2d4a225e1
renan <renan@interprocess.com.br>
2022-05-17 11:38:18 -0300

    feat: add interface for publish message bus

--- AwesomeShop.Services.Orders.Infrastructure/Extensions.cs
+++ AwesomeShop.Services.Orders.Infrastructure/Extensions.cs
@@ -1,8 +1,12 @@
 ﻿using AwesomeShop.Services.Orders.Infrastructure.db.mongo;
+using AwesomeShop.Services.Orders.Infrastructure.MessageBus;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace AwesomeShop.Services.Orders.Infrastructure
 {
@@ -41,6 +45,55 @@ namespace AwesomeShop.Services.Orders.Infrastructure
             });
 
             return services;
+        }
+
+        public static IServiceCollection AddMessageBus(this IServiceCollection services)
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5673
+            };
+
+            var connection = connectionFactory.CreateConnection("order-service-producer");
+
+            services.AddSingleton(new ProducerConnection(connection));
+            services.AddSingleton<IMessageBusCllient, MessageBusCllient>();
+
+            return services;
+        }
+
+        public static string ToDashCase(this string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException(nameof(text));
+            }
+
+            if (text.Length < 2)
+            {
+                return text;
+            }
+
+            var sb = new StringBuilder();
+            sb.Append(char.ToLowerInvariant(text[0]));
+
+            for (int i = 1; i < text.Length; i++)
+            {
+                char c = text[i];
+
+                if (char.IsUpper(c))
+                {
+                    sb.Append('-');
+                    sb.Append(char.ToLowerInvariant(c));
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
---
+++ AwesomeShop.Services.Orders.Infrastructure/MessageBus/IMessageBusCllient.cs
@@ -0,0 +1,7 @@
+﻿namespace AwesomeShop.Services.Orders.Infrastructure.MessageBus
+{
+    public interface IMessageBusCllient
+    {
+        void Publish(object message, string routingKey, string exchange);
+    }
+}
