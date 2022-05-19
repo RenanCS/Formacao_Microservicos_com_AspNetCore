309ae6f8f691429b3adc46b6857f5b81eb4188d7
renan <renan@interprocess.com.br>
2022-05-19 11:52:30 -0300

    chore: add rule into handle order

--- AwesomeShop.Services.Orders.Application/Commands/Handlers/AddOrderHandler.cs
+++ AwesomeShop.Services.Orders.Application/Commands/Handlers/AddOrderHandler.cs
@@ -1,9 +1,13 @@
 ﻿using AwesomeShop.Services.Orders.Application.Dtos.InputModels;
+using AwesomeShop.Services.Orders.Application.Dtos.IntegrationDtos;
 using AwesomeShop.Services.Orders.Core.Repositories;
 using AwesomeShop.Services.Orders.Infrastructure;
 using AwesomeShop.Services.Orders.Infrastructure.MessageBus;
+using AwesomeShop.Services.Orders.Infrastructure.ServiceDiscovery;
 using MediatR;
+using Newtonsoft.Json;
 using System;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,15 +17,28 @@ namespace AwesomeShop.Services.Orders.Application.Commands.Handlers
     {
         private readonly IOrderRepository _orderRepository;
         private readonly IMessageBusCllient _messageBusCllient;
-        public AddOrderHandler(IOrderRepository orderRepository, IMessageBusCllient messageBusClient)
+        private readonly IServiceDiscoveryService _serviceDiscoveryService;
+        public AddOrderHandler(IOrderRepository orderRepository, IMessageBusCllient messageBusClient, IServiceDiscoveryService serviceDiscoverySerice)
         {
             _orderRepository = orderRepository;
             _messageBusCllient = messageBusClient;
+            _serviceDiscoveryService = serviceDiscoverySerice;
         }
 
         public async Task<Guid> Handle(AddOrderCommand request, CancellationToken cancellationToken)
         {
             var order = request.FactoryAddOrderCommandToOrderEntity();
+
+            var customerUrl = await _serviceDiscoveryService.GetServiceUri("CustumerServices", $"api/customers/{order.Customer.Id}");
+            if (customerUrl != null)
+            {
+                var httpClient = new HttpClient();
+                var result = await httpClient.GetAsync(customerUrl);
+                var stringResult = await result.Content.ReadAsStringAsync();
+                var customerDto = JsonConvert.DeserializeObject<GetCustomerByIdDto>(stringResult);
+                Console.WriteLine($"******AddOrderHandle  => CustomerServices => {stringResult} ***********");
+            }
+
 
             await _orderRepository.AddAsync(order);
 
